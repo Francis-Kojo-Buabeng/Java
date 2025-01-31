cab34dbd5a0f67f798eb49fe72e129543888d08b
Francis Kojo Buabeng <myallcodes1@gmail.com>
2025-01-31 16:14:06 +0000

    commit all changes

--- ReadingInput/ReadUserInput.java
+++ ReadingInput/ReadUserInput.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 
 public class ReadUserInput {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("What's your name: ");
-        String name = scanner.next().toUpperCase();
+        Scanner scanner = new Scanner(System.in); //Scanner instantiation
+        System.out.print("What's your name: ");
+        String name = scanner.nextLine().trim();
 
-        System.out.println("Enter your Age: ");
+        System.out.print("Enter your Age: ");
         Byte age = scanner.nextByte();
         System.out.println(name+", you're "+age+" years old");
     }
