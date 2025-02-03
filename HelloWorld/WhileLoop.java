79eec01cc49081cbe4dc1486916c468b6aaea4ac
Francis Kojo Buabeng <myallcodes1@gmail.com>
2025-02-03 21:17:42 +0000

    while loop added

---
+++ HelloWorld/WhileLoop.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class WhileLoop {
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String guess = "";
+
+        while(!guess.equals("QUIT") ){
+            System.out.print("Guess a Word: ");
+            guess = scanner.next().toUpperCase();   
+        }
+
+        scanner.close();
+    }
+}
